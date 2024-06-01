da1655b8d93e04e23db90d728c20e0e26bc91ec7
Nagma Kazi <Nagma.Kazi@necsws.com>
2024-06-01 11:18:45 +0100

    initial commit

---
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -0,0 +1,58 @@
+package com.crio.qcalc;
+
+public class StandardCalculator {
+    private int result;
+
+    public static void getVersion(){
+        System.out.println("Standard Calculator 1.0");
+    }
+
+    
+public int getResult() {
+    return result;
+}
+
+private void setResult(int value){
+    this.result=value;
+}
+
+public void clearResult(){
+    result=0;
+}
+
+
+public void printResult(){
+    System.out.println("Standard Calculator result ="+getResult()); 
+}
+
+
+
+
+   
+public void add(int num1, int num2){
+    
+    result = num1+num2;
+
+}
+ 
+
+public void subtract(int num1, int num2){
+    result =num1-num2;
+    
+}
+
+
+public void multiply(int num1, int num2){
+    result =num1 * num2;
+   
+}
+
+
+public void divide(int num1, int num2){
+    result = num1 / num2;
+    
+}
+
+
+
+}
